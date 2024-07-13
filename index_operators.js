b9d0cbb254d466545620e55e1740c197d1f01a8b
Dattu44 <addankijnanadath@gmail.com>
2024-07-13 21:23:10 +0530

    Add files via upload

---
+++ index_operators.js
@@ -0,0 +1,34 @@
+//data types
+var str = "Hello, World!";
+var num = 42;
+var bool = true;
+var und;
+var nul = null;
+
+
+console.log( typeof str);
+console.log( typeof num);
+console.log( typeof bool);
+console.log( typeof und);
+console.log( typeof nul); 
+
+// Mathematical Operators
+var a = 10;
+var b = 5;
+
+console.log("Addition: " + (a + b))
+console.log("Subtraction: " + (a - b))
+console.log("Multiplication: " + (a * b))
+console.log("Division: " + (a / b))
+console.log("Modulus: " + (a % b))
+
+// Comparator Operators
+
+console.log("Equality : " + (a == b))
+console.log("Strict Equality : " + (a === b))
+console.log("Inequality : " + (a != b))
+console.log("Strict Inequality : " + (a !== b))
+console.log("Greater than : " + (a > b))
+console.log("Less than : " + (a < b))
+console.log("Less than r equal : " + (a <=b))
+console.log("greater than r equal : " + (a >= b));
